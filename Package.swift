e27baaaee8382ae8d07f299d7bef7a86f448f4fa
Francisco Rosso <frosso@zafirus.tech>
2025-09-08 00:13:27 -0300

    change name
    change name

--- Package.swift
+++ Package.swift
@@ -3,21 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "ElegantCalendar",
+    name: "IunokCalendar",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "ElegantCalendar",
-            targets: ["ElegantCalendar"]),
+            name: "IunokCalendar",
+            targets: ["IunokCalendar"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ThasianX/ElegantPages", from: "1.4.1")
     ],
     targets: [
         .target(
-            name: "ElegantCalendar",
+            name: "IunokCalendar",
             dependencies: ["ElegantPages"])
     ]
 )
